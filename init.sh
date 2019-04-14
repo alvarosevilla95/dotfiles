b1e572ced882d8ea59a2251c713add5aba22d257
Alvaro Sevilla <alvaro@chainalysis.com>
2019-04-14 18:11:23 +0100

    added bootstrap commit

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# general
+sudo pacman -S yay &&
+yay -S firefox;
+
+# Xmodmap 
+ln -s ~/.dotfiles/Xmodmap ~/.Xmodmap &&
+xmodmap ~/.Xmodmap;
+
+# i3
+ln -s ~/.dotfiles/i3-config  ~/.i3/config;
+ln -s ~/.dotfiles/Xresources  ~/.Xresources;
+
+# zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
+ln -s ~/.dotfiles/zshrc ~/.zshrc &&
+ln -s ~/.dotfiles/cbira.zsh-theme ~/.oh-my-zsh/custom/themes/cbira.zsh-theme;
+
+# vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
+ln -s ~/.dotfiles/vimrc ~/.vimrc &&
+ln -s ~/.dotfiles/lightline-manjaro.vim ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/manjaro.vim;
+
+# tmux
+yay -S tmux &&
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf;
