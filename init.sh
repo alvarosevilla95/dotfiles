1fd3cbb462fc05384389aa8d7c521c7d511b170d
Alvaro Sevilla <alvaro@chainalysis.com>
2019-04-15 00:20:14 +0100

    force link creation

--- init.sh
+++ init.sh
@@ -4,24 +4,33 @@
 sudo pacman -S yay &&
 
 # Xmodmap 
+rm -rf ~/.Xmodmap;
 ln -s ~/.dotfiles/Xmodmap ~/.Xmodmap &&
 xmodmap ~/.Xmodmap;
 
 # i3
+rm -rf ~/.i3/config;
+ln -s ~/.dotfiles/Xmodmap ~/.Xmodmap &&
 ln -s ~/.dotfiles/i3-config  ~/.i3/config;
 mkdir -p ~/.config/i3status;
+rm -rf ~/.config/i3status/config;
 ln -s ~/.dotfiles/i3status-config ~/.config/i3status/config;
+rm -rf ~/.Xresources;
 ln -s ~/.dotfiles/Xresources  ~/.Xresources;
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" &&
+rm -rf ~/.zshrc;
 ln -s ~/.dotfiles/zshrc ~/.zshrc &&
+rm -rf ~/.oh-my-zsh/custom/themes/cbira.zsh-theme;
 ln -s ~/.dotfiles/cbira.zsh-theme ~/.oh-my-zsh/custom/themes/cbira.zsh-theme;
 
 # vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
+rm -rf ~/.vimrc;
 ln -s ~/.dotfiles/vimrc ~/.vimrc &&
 vim -c :PlugInstall -c :q -c :q &&
+rm -rf ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/manjaro.vim;
 ln -s ~/.dotfiles/lightline-manjaro.vim ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/manjaro.vim;
 yay -S ripgrep &&
 
@@ -31,7 +40,9 @@ ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf;
 
 # ssh
 mkdir -p ~/.config/systemd/user/ &&
-ln -s ~/.dotfiles/ssh-config ~/.ssh/config
+rm -rf ~/.ssh/config;
+ln -s ~/.dotfiles/ssh-config ~/.ssh/config;
+rm -rf ~/.config/systemd/user/ssh-agent.service &&
 ln -s ~/.dotfiles/ssh-agent.service ~/.config/systemd/user/ssh-agent.service &&
 systemctl --user enable ssh-agent &&
 systemctl --user start ssh-agent;
