d4a3bf60d041d468da84d82107a3d66ac9593806
Alvaro Sevilla <alvaro@chainalysis.com>
2019-04-15 01:13:55 +0100

    simplify init.sh

--- init.sh
+++ init.sh
@@ -10,7 +10,6 @@ yes | yay -S tmux;
 
 # vim
 mkdir -p ~/.vim/autoload/; cp ~/.dotfiles/vim.plug ~/.vim/autoload/plug.vim;
-vim -c :PlugInstall -c :q -c :q;
 
 # zsh
 sh -c ./install-oh-my-zsh.sh;
@@ -26,7 +25,6 @@ rm -rf ~/.Xresources; ln -s ~/.dotfiles/Xresources ~/.Xresources;
 rm -rf ~/.zshrc; ln -s ~/.dotfiles/zshrc ~/.zshrc;
 rm -rf ~/.oh-my-zsh/custom/themes/cbira.zsh-theme; ln -s ~/.dotfiles/cbira.zsh-theme ~/.oh-my-zsh/custom/themes/cbira.zsh-theme;
 rm -rf ~/.vimrc; ln -s ~/.dotfiles/vimrc ~/.vimrc;
-rm -rf ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/manjaro.vim; ln -s ~/.dotfiles/lightline-manjaro.vim ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/manjaro.vim;
 rm -rf ~/.tmux.conf; ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf;
 rm -rf ~/.ssh/config; mkdir ~/.ssh/; ln -s ~/.dotfiles/ssh-config ~/.ssh/config;
 rm -rf ~/.config/systemd/user/ssh-agent.service; mkdir -p ~/.config/systemd/user/; ln -s ~/.dotfiles/ssh-agent.service ~/.config/systemd/user/ssh-agent.service;
@@ -39,5 +37,8 @@ systemctl --user start ssh-agent;
 mkdir -p ~/Dev/go/{src,bin,pkg};
 
 xmodmap ~/.Xmodmap;
+
+vim -c :PlugInstall -c :q -c :q;
+rm -rf ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/manjaro.vim; ln -s ~/.dotfiles/lightline-manjaro.vim ~/.vim/plugged/lightline.vim/autoload/lightline/colorscheme/manjaro.vim;
 
 chsh -s /usr/bin/zsh;
